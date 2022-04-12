a27950491605e17d3d41fb7eb8f1ed35cf7d98dc
shyam <shyam@DESKTOP-EIOBN08>
2022-04-12 12:30:33 +0530

    Commit 2

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	
+	@Test
+	public void demoTest() {
+		System.out.println("Second Push");
+		System.out.println("Second Push");
+	
+	}
+}
